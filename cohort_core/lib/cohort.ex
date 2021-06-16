4be0096e10ac38579eb83e20134bb203ee1772bf
Marcin Lewandowski <marcin.lewandowski@swmansion.com>
2021-06-16 14:03:07 +0200

    Removed hello worlds

--- cohort_core/lib/cohort.ex
+++ cohort_core/lib/cohort.ex
@@ -1,18 +1,3 @@
 defmodule Cohort do
-  @moduledoc """
-  Documentation for `Cohort`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cohort.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- cohort_transport_gun/lib/cohort/transport/gun.ex
+++ cohort_transport_gun/lib/cohort/transport/gun.ex
@@ -1,18 +1,3 @@
 defmodule Cohort.Transport.Gun do
-  @moduledoc """
-  Documentation for `Cohort.Transport.Gun`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cohort.Transport.Gun.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
